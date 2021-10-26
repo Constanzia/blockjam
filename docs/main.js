5866bcb52218920a685cb14eb9ba63fd0b2249d1
Constanzia <83115706+Constanzia@users.noreply.github.com>
2021-10-26 16:35:05 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -11,6 +11,7 @@ let min = date.getMinutes();
 
 const info = document.getElementById("info");
 const infobtn = document.getElementById("infobtn");
+const infocir = document.getElementById("cir1");
 infobtn.onclick = function() {
     if (info.style.display !== "none") {
         info.style.display = "none";
@@ -18,13 +19,61 @@ infobtn.onclick = function() {
         info.style.display = "block";
     }
 };
+infocir.onclick = function() {
+    if (info.style.display !== "block") {
+        info.style.display = "block";
+    }
+    // } else {
+    //     info.style.display = "none";
+    // }
+};
 
 const credit = document.getElementById("credit");
 const creditbtn = document.getElementById("creditbtn");
+const credcir = document.getElementById("cir2");
 creditbtn.onclick = function() {
-    if (credit.style.display !== "none") {
-        credit.style.display = "none";
-    } else {
+    if (credit.style.display !== "block") {
         credit.style.display = "block";
+    } else {
+        credit.style.display = "none";
     }
 };
+credcir.onclick = function() {
+    if (credit.style.display !== "block") {
+        credit.style.display = "block";
+    }
+    // else {
+    //     credit.style.display = "none";
+    // }
+};
+
+
+
+
+var elementSelected;
+var mouseX, mouseY;
+var zIndex = 109;
+document.querySelectorAll(".move").forEach(function(element) {
+    // element.style.left = 400;
+    element.addEventListener('mousedown', function() {
+        elementSelected = element;
+        // mouseX = event.clientX - parseInt(getComputedStyle(elementSelected).left);
+        // mouseY = event.clientY - parseInt(getComputedStyle(elementSelected).top);
+        // move this element to top layer
+        elementSelected.style['z-index'] = zIndex++;
+        // document.querySelector(".container").appendChild(elementSelected);
+    })
+
+});
+
+document.addEventListener('mousemove', function(event) {
+    if (elementSelected !== undefined) {
+        elementSelected.style.left = event.clientX - 160 + 'px';
+        elementSelected.style.top = event.clientY - 70 + 'px';
+    }
+});
+
+document.addEventListener('mouseup', function() {
+    // elementSelected.style['z-index'] = 109;
+    elementSelected = undefined;
+});
